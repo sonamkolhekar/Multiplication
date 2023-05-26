1477f47052930822a06a9fd7f0576195c9ac49e3
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 11:20:18 +0530

    3 line added from eclips

--- src/com/example/Multi.java
+++ src/com/example/Multi.java
@@ -3,7 +3,12 @@ package com.example;
 public class Multi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 10;
+		System.out.println("Multiplicatiin : " + (a * b));
+		a = 20;
+	    b = 20;
+		System.out.println("Multiplicatiin : " + (a * b));
 
 	}
 
